3409ebe6275576c9e1ebe450a469ed9969856264
Rabia-Jamil <rabiajamil251@gmail.com>
2019-10-29 12:43:16 +0500

    Add files via upload

---
+++ src/models/User.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+
+// Secret Config
+const secret_key = require('../../config/keys').JWT_SECRET;
+
+const Schema = mongoose.Schema;
+
+// Create Schema
+const UserSchema = new Schema({
+  first_name: {
+    type: String,
+    required: true
+  },
+  last_name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  role: {
+    type: String,
+    required: true
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = User = mongoose.model('users', UserSchema);

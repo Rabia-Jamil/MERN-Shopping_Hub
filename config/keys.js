8d0624d669bd8ed639820fbba79eccd99ef239f5
Rabia-Jamil <rabiajamil251@gmail.com>
2019-11-04 22:00:43 +0500

    Add files via upload

---
+++ config/keys.js
@@ -0,0 +1,8 @@
+module.exports = {
+    PORT:"4000",
+    MONGODB_URL:"mongodb://profile-wishlist:abc123456!@cluster0-shard-00-00-fvnyo.mongodb.net:27017,cluster0-shard-00-01-fvnyo.mongodb.net:27017,cluster0-shard-00-02-fvnyo.mongodb.net:27017/shopping_hub?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority",
+    JWT_SECRET: "thisIsMySecretKey",
+    cloudinary : {cloud_name: "", 
+    api_key: "671755859622817", 
+    api_secret: "8uU3X_xD3xqErS0Jnx7gnrcK7aU"},
+}
